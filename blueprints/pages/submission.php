817bb96a778edd70df2a18df0e4abe53e9cbb588
tobimori <tobias@moeritz.io>
2024-03-05 19:20:31 +0100

    fix extracting page from url, fix key field icon

--- blueprints/pages/submission.php
+++ blueprints/pages/submission.php
@@ -4,18 +4,17 @@ use Kirby\Cms\App;
 use Kirby\Toolkit\Str;
 
 return function () {
-	$path = App::instance()->request()->path()->data()[2];
-	$page = App::instance()->site()->findPageOrDraft(Str::replace($path, '+', '/'));
-
-	if ($page->intendedTemplate()->name() !== 'submission') {
-		return false;
-	}
+	$path = App::instance()->request()->url()->toString();
+	$matches = Str::match($path, "/pages\/([a-zA-Z0-9+]+)\/?/m");
+	$page = App::instance()->site()->findPageOrDraft(Str::replace($matches[1], '+', '/'));
 
 	$blueprint = [];
-	$fields = $page->parent()->fields();
-	foreach ($fields as $field) {
-		$key = $field->field()->key()->or($field->field()->id())->value();
-		$blueprint[$key] = $field->submissionBlueprint() ?? false;
+	if ($page?->intendedTemplate()?->name() === 'submission') {
+		$fields = $page->parent()->fields();
+		foreach ($fields as $field) {
+			$key = $field->field()->key()->or($field->field()->id())->value();
+			$blueprint[$key] = $field->submissionBlueprint() ?? false;
+		}
 	}
 
 	return [
