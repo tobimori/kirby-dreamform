d27ce9656ffcfefbf2c3c9036f183e9d109cd89f
tobimori <tobias@moeritz.io>
2024-03-31 21:02:21 +0200

    don't show form field for permissionless users

--- blueprints/fields/form.php
+++ blueprints/fields/form.php
@@ -3,6 +3,12 @@
 use Kirby\Cms\App;
 
 return function () {
+	if (!App::instance()->user()->role()->permissions()->for('tobimori.dreamform', 'accessForms')) {
+		return [
+			'type' => 'hidden',
+		];
+	}
+
 	$page = App::instance()->option('tobimori.dreamform.page', 'page://forms');
 
 	return [
