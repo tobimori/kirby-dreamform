984e7cd27cd1e7a0bb5cb4a27b2660735797232c
tobimori <tobias@moeritz.io>
2024-04-05 14:51:25 +0200

    fix license activation

--- src/sections/dreamform-license.vue
+++ src/sections/dreamform-license.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, useSection } from "kirbyuse";
+import { ref, useApp, usePanel, useSection } from "kirbyuse";
 import { section } from "kirbyuse/props";
 
 const props = defineProps(section);
@@ -7,93 +7,111 @@ const props = defineProps(section);
 const activated = ref(true);
 const local = ref(false);
 
-(async () => {
-  const { load } = useSection();
-  const response = await load({
-    parent: props.parent,
-    name: props.name,
-  });
+const loadSection = async () => {
+	const { load } = useSection();
+	const response = await load({
+		parent: props.parent,
+		name: props.name,
+	});
 
-  activated.value = response.activated;
-  local.value = response.local;
+	activated.value = response.activated;
+	local.value = response.local;
+};
+
+const app = useApp();
+const panel = usePanel();
+const openDialog = () => {
+	app.$dialog("dreamform/activate", {
+		on: {
+			success(t) {
+				panel.dialog.close();
+				panel.notification.success(t.message);
+				loadSection();
+			},
+		},
+	});
+};
+
+(() => {
+	loadSection();
 })();
 </script>
 
 <template>
-  <k-section class="df-license-section" v-if="!activated">
-    <div class="df-license-section-wrapper">
-      <a
-        href="https://plugins.andkindness.com/dreamform"
-        target="_blank"
-        class="df-logo"
-      >
-        <k-icon type="dreamform" class="" />
-        <h1>DreamForm</h1>
-      </a>
-      <h2
-        v-text="
-          $t(
-            local
-              ? 'dreamform.ready-to-activate'
-              : 'dreamform.submissions-demo-mode'
-          )
-        "
-      ></h2>
-    </div>
-    <a href="https://plugins.andkindness.com/dreamform/pricing" target="_blank">
-      {{ $t("dreamform.buy-license") }}
-    </a>
-    <k-button
-      size="sm"
-      theme="info"
-      variant="filled"
-      icon="key"
-      @click="dialog()"
-    >
-      {{ $t("dreamform.activate-license") }}
-    </k-button>
-  </k-section>
+	<k-section class="df-license-section" v-if="!activated">
+		<div class="df-license-section-wrapper">
+			<a
+				href="https://plugins.andkindness.com/dreamform"
+				target="_blank"
+				class="df-logo"
+			>
+				<k-icon type="dreamform" class="" />
+				<h1>DreamForm</h1>
+			</a>
+			<h2
+				v-text="
+					$t(
+						local
+							? 'dreamform.ready-to-activate'
+							: 'dreamform.submissions-demo-mode'
+					)
+				"
+			></h2>
+		</div>
+		<a href="https://plugins.andkindness.com/dreamform/pricing" target="_blank">
+			{{ $t("dreamform.buy-license") }}
+		</a>
+		<k-button
+			size="sm"
+			theme="info"
+			variant="filled"
+			icon="key"
+			@click="openDialog()"
+		>
+			{{ $t("dreamform.activate-license") }}
+		</k-button>
+	</k-section>
 </template>
 
 <style lang="scss">
 .df-logo {
-  display: flex;
-  align-items: center;
-  color: #1b4493;
-  margin-right: 1rem;
-  font-weight: var(--font-semi);
+	display: flex;
+	align-items: center;
+	color: #1b4493;
+	margin-right: 1rem;
+	font-weight: var(--font-semi);
 
-  .k-icon {
-    width: 1rem;
-    height: 1rem;
-    margin-right: 0.5rem;
-  }
+	.k-icon {
+		width: 1rem;
+		height: 1rem;
+		margin-right: 0.5rem;
+	}
 }
 
 .df-license-section {
-  background: var(--color-blue-300);
-  padding: var(--spacing-1) var(--spacing-1);
-  border-radius: var(--rounded-lg);
-  justify-content: space-between;
+	background: var(--color-blue-300);
+	padding: var(--spacing-1) var(--spacing-1);
+	border-radius: var(--rounded-lg);
+	justify-content: space-between;
 
-  &,
-  &-wrapper {
-    display: flex;
-    align-items: center;
-  }
+	&,
+	&-wrapper {
+		display: flex;
+		align-items: center;
+	}
 
-  &-wrapper {
-    padding: var(--spacing-1);
-  }
+	&-wrapper {
+		padding: var(--spacing-1);
+	}
 
-  a:not(.df-logo) {
-    display: block;
-    color: var(--color-blue-800);
-    text-decoration: underline;
-    text-decoration-color: currentColor;
-    text-underline-offset: 0.125rem;
-    margin-right: 0.75rem;
-    margin-left: auto;
-  }
+	a:not(.df-logo) {
+		display: block;
+		color: var(--color-blue-800);
+		text-decoration: underline;
+		text-decoration-color: currentColor;
+		text-underline-offset: 0.125rem;
+		margin-right: 0.75rem;
+		margin-left: auto;
+	}
 }
 </style>
