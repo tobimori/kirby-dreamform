991fc2175926b04fff9935cd6f5a16eec75939e6
tobimori <tobias@moeritz.io>
2024-04-23 20:29:01 +0200

    add mailchimp action logic

--- classes/Actions/MailchimpAction.php
+++ classes/Actions/MailchimpAction.php
@@ -12,6 +12,9 @@ use tobimori\DreamForm\Models\FormPage;
 
 class MailchimpAction extends Action
 {
+	/**
+	 * Returns the Blocks fieldset blueprint for the actions' settings
+	 */
 	public static function blueprint(): array
 	{
 		$lists = static::cache(
@@ -58,48 +61,110 @@ class MailchimpAction extends Action
 		];
 	}
 
+	/**
+	 * Subscribe the user to the Mailchimp list
+	 */
 	public function run(): void
 	{
+		$list = $this->block()->list()->value();
+		$mapping = $this->block()->fieldMapping()->toObject();
+
+		// get the email address from the submission
+		$email = $this->submission()->valueForId($mapping->emailAddress()->value())->value();
+		if (!$email) {
+			return;
+		}
+
+		// get data for merge fields from the submission
+		$mergeFields = [];
+		foreach ($mapping->data() as $mergeField => $fieldId) {
+			if (A::has(['emailaddress', 'tags', 'tagsfield', 'tagsstatic'], $mergeField) || !$fieldId) {
+				continue;
+			}
+
+			if ($value = $this->submission()->valueForId($fieldId)?->value()) {
+				$mergeFields[Str::upper($mergeField)] = $value;
+			}
+		}
+
+		// get tags from submission
+		$tags = A::map(
+			($mapping->tags()->value() === 'static' ?
+				$mapping->tagsStatic()->value() :
+				$this->submission()->valueForId($mapping->tagsField()->value())?->split()) ?? [],
+			fn ($tag) => isset(static::getTags($list)[$tag]) ? static::getTags($list)[$tag] : $tag
+		);
+
+		// subscribe or update the user
+		$hash = md5(strtolower($email));
+		static::request('PUT', "/lists/{$list}/members/{$hash}?skip_merge_validation=true", [
+			'email_address' => $email,
+			'status_if_new' => $this->block()->doubleOptIn()->toBool() ? 'pending' : 'subscribed',
+			'merge_fields' => $mergeFields,
+			'tags' => $tags,
+
+			// this will be included if it's already set (and enabled in the config)
+			'ip_signup' => $this->submission()->metadata()->ip()?->value(),
+			'language' => App::instance()->languageCode()
+		]);
 	}
 
+	/**
+	 * Returns an array of static segments/tags for the list
+	 */
+	protected static function getTags(string $list): array
+	{
+		// Retrieve segment tags from the API
+		$segments = static::cache(
+			"{$list}.segments",
+			fn () => static::request('GET', "/lists/{$list}/segments?count=1000")?->json()
+		)['segments'];
+
+		// Create an array of segment tags
+		$tags = A::reduce($segments, fn ($prev, $segment) => A::merge(
+			$prev,
+			$segment['type'] === 'static' ? [
+				"id_{$segment['id']}" => $segment['name']
+			] : []
+		), []);
+
+		return $tags;
+	}
+
+	/**
+	 * Returns the available merge fields for the list
+	 * from the mailchimp API
+	 */
 	public static function fieldMapping(FormPage $page, string $list): array
 	{
-		$fields = static::cache(
+		// Retrieve merge fields from the API
+		$mergeFields = static::cache(
 			"{$list}.fields",
 			fn () => static::request('GET', "/lists/{$list}/merge-fields")?->json()
 		);
 
-		$options = FormPage::getFields();
+		// Create the base blueprint with the email address field
 		$blueprint = [
-			'email_address' => [
+			'emailAddress' => [
 				'label' => t('email'),
 				'type' => 'select',
 				'required' => true,
-				'options' => $options,
+				'options' => $options = FormPage::getFields(),
 			],
 		];
 
-		foreach ($fields['merge_fields'] as $mergeField) {
-			// we don't support the following merge fields (for now) since they have additional validation
-			// TODO: implement matching fields for these, and filter by them in the field mapping
-			if (A::has(['address', 'birthday', 'date', 'imageurl'], $mergeField['type'])) {
-				continue;
+		// Add supported merge fields to the blueprint
+		foreach ($mergeFields['merge_fields'] as $mergeField) {
+			if (!A::has(['address', 'birthday', 'date', 'imageurl'], $mergeField['type'])) {
+				$blueprint[Str::lower($mergeField['tag'])] = [
+					'label' => "{$mergeField['name']} ({$mergeField['tag']})",
+					'type' => 'select',
+					'options' => $options,
+				];
 			}
-
-			$blueprint[Str::lower($mergeField['tag'])] = [
-				'label' => "{$mergeField['name']} ({$mergeField['tag']})",
-				'type' => 'select',
-				'options' => $options,
-			];
 		}
 
-		$tags = A::reduce(static::cache(
-			"{$list}.segments",
-			fn () => static::request('GET', "/lists/{$list}/segments?count=1000")?->json()
-		)['segments'], fn ($prev, $segment) => A::merge($prev, $segment['type'] === 'static' ? [
-			"id_{$segment['id']}" => $segment['name']
-		] : []), []);
-
+		// Add tags-related fields to the blueprint
 		$blueprint = A::merge($blueprint, [
 			'line' => [
 				'type' => 'line'
@@ -127,7 +192,7 @@ class MailchimpAction extends Action
 			'tagsStatic' => [
 				'label' => t('dreamform.tags'),
 				'type' => 'multiselect',
-				'options' => $tags,
+				'options' => static::getTags($list),
 				'width' => '2/3',
 				'when' => [
 					'tags' => 'static'
@@ -135,12 +200,12 @@ class MailchimpAction extends Action
 			]
 		]);
 
+		// Create and return the form fields based on the blueprint and page model
 		return (new Form([
 			'fields' => $blueprint,
 			'model' => $page
 		]))->fields()->toArray();
 	}
-
 	/**
 	 * Get the API key for the Mailchimp API
 	 **/
@@ -154,11 +219,14 @@ class MailchimpAction extends Action
 		return $option;
 	}
 
+	/**
+	 * Send a Mailchimp API request
+	 */
 	public static function request(string $method, string $url, array $data = []): Remote
 	{
 		if ($method !== 'GET') {
 			$params = [
-				'data' => Json::encode($data),
+				'data' => Json::encode(A::filter($data, fn ($value) => $value !== null)),
 				'headers' => [
 					'Content-Type' => 'application/json'
 				]
@@ -170,12 +238,18 @@ class MailchimpAction extends Action
 		], $params ?? []));
 	}
 
+	/**
+	 * Get the Mailchimp API URL
+	 */
 	public static function apiUrl(): string
 	{
 		$dc = Str::after(static::apiKey(), '-');
 		return "https://{$dc}.api.mailchimp.com/3.0";
 	}
 
+	/**
+	 * Returns true if the Mailchimp action is available
+	 */
 	public static function isAvailable(): bool
 	{
 		if (!static::apiKey()) {
@@ -188,6 +262,9 @@ class MailchimpAction extends Action
 		});
 	}
 
+	/**
+	 * Returns the actions' blueprint group
+	 */
 	public static function group(): string
 	{
 		return 'newsletter';
