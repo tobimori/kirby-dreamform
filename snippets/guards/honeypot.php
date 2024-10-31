8e16e9e32512fb363ecc4e20f3129541c114b4b3
gitlabisbetterthangithub <40391197+gitlabisbetterthangithub@users.noreply.github.com>
2024-10-31 22:32:37 +1000

    Update honeypot.php
    Fixes #95

--- snippets/guards/honeypot.php
+++ snippets/guards/honeypot.php
@@ -5,4 +5,4 @@
  * @var tobimori\DreamForm\Guards\HoneypotGuard $guard
  */ ?>
 
-<input type="text" style="position:absolute;width:1px;height:1px;padding:0;margin:-1px;overflow:hidden;clip:rect(0,0,0,0);white-space:nowrap;border-width:0;" aria-hidden tabindex="-1" name="<?= $guard->fieldName() ?>">
+<input type="text" style="position:absolute;width:1px;height:1px;padding:0;margin:-1px;overflow:hidden;clip:rect(0,0,0,0);white-space:nowrap;border-width:0;" aria-hidden="true" tabindex="-1" name="<?= $guard->fieldName() ?>">
