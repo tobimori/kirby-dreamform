7fde851a005a3454cd65ab5f662f1fdd73217692
tobimori <tobias@moeritz.io>
2025-10-10 17:18:36 +0200

    bump

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,8 +3,8 @@
 return array(
 	'root' => array(
 		'name' => 'tobimori/kirby-dreamform',
-		'pretty_version' => '2.0.0',
-		'version' => '2.0.0.0',
+		'pretty_version' => '2.0.1',
+		'version' => '2.0.1.0',
 		'reference' => null,
 		'type' => 'kirby-plugin',
 		'install_path' => __DIR__ . '/../../',
@@ -22,8 +22,8 @@ return array(
 			'dev_requirement' => false,
 		),
 		'tobimori/kirby-dreamform' => array(
-			'pretty_version' => '2.0.0',
-			'version' => '2.0.0.0',
+			'pretty_version' => '2.0.1',
+			'version' => '2.0.1.0',
 			'reference' => null,
 			'type' => 'kirby-plugin',
 			'install_path' => __DIR__ . '/../../',
