199437985b74b0bd8e0e2eb03ba6b2c1fdcee0f9
tobimori <tobias@moeritz.io>
2025-11-04 13:15:20 +0100

    bump version number

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,34 +1,32 @@
-<?php
-
-return array(
-	'root' => array(
-		'name' => 'tobimori/kirby-dreamform',
-		'pretty_version' => '2.0.1',
-		'version' => '2.0.1.0',
-		'reference' => null,
-		'type' => 'kirby-plugin',
-		'install_path' => __DIR__ . '/../../',
-		'aliases' => array(),
-		'dev' => false,
-	),
-	'versions' => array(
-		'getkirby/composer-installer' => array(
-			'pretty_version' => '1.2.1',
-			'version' => '1.2.1.0',
-			'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
-			'type' => 'composer-plugin',
-			'install_path' => __DIR__ . '/../getkirby/composer-installer',
-			'aliases' => array(),
-			'dev_requirement' => false,
-		),
-		'tobimori/kirby-dreamform' => array(
-			'pretty_version' => '2.0.1',
-			'version' => '2.0.1.0',
-			'reference' => null,
-			'type' => 'kirby-plugin',
-			'install_path' => __DIR__ . '/../../',
-			'aliases' => array(),
-			'dev_requirement' => false,
-		),
-	),
+<?php return array(
+    'root' => array(
+        'name' => 'tobimori/kirby-dreamform',
+        'pretty_version' => '2.0.2',
+        'version' => '2.0.2.0',
+        'reference' => null,
+        'type' => 'kirby-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'getkirby/composer-installer' => array(
+            'pretty_version' => '1.2.1',
+            'version' => '1.2.1.0',
+            'reference' => 'c98ece30bfba45be7ce457e1102d1b169d922f3d',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/../getkirby/composer-installer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'tobimori/kirby-dreamform' => array(
+            'pretty_version' => '2.0.2',
+            'version' => '2.0.2.0',
+            'reference' => null,
+            'type' => 'kirby-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
 );
