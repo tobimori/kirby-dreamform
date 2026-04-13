b423e07182211760e506b69462860d8a7f0131be
tobimori <tobias@moeritz.io>
2026-04-13 11:28:07 +0200

    fix(htmx): use form ID selector for hx-disabled-elt (closes #166)

--- classes/Models/FormPage.php
+++ classes/Models/FormPage.php
@@ -56,7 +56,7 @@ class FormPage extends BasePage
 
 		$htmx = [
 			'hx-post' => $this->url(),
-			'hx-disabled-elt' => 'find button',
+			'hx-disabled-elt' => "#{$this->elementId()} button",
 			'hx-swap' => 'outerHTML show:top',
 			'hx-vals' => Json::encode(array_filter([
 				'dreamform:page' => Htmx::encrypt($page->uuid()->toString()),
